0d35ccf30bafd5f20916c4526f25a05919d7f47e
Shadow0144 <CorbinCogswell@gmail.com>
2021-02-19 01:33:00 -0600

    Adding files

---
+++ PlanetTextureFixMod/Patches.cs
@@ -0,0 +1,21 @@
+﻿using Harmony;
+
+namespace PlanetTextureFixMod
+{
+    public class Patches
+    {
+        [HarmonyPatch(typeof(Db))]
+        [HarmonyPatch("Initialize")]
+        public class Db_Initialize_Patch
+        {
+            public static void Postfix()
+            {
+                Debug.Log("Fixing planet textures...");
+
+                // Enter code here
+
+                Debug.Log("...Planet textures fixed.");
+            }
+        }
+    }
+}
